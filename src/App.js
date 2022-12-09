c8f6af52ddb9f787dc1c3250d1bf0806309b4ab7
Don-Sebastian <donsebastian1333@gmail.com>
2022-12-09 09:48:42 +0530

    fri commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable react/no-typos */
 /* eslint-disable react/jsx-filename-extension */
